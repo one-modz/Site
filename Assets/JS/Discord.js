4e0ab9304e1f5f55b3782808a62a779e9c4b8562
ONE MODZ <129001939+one-modz@users.noreply.github.com>
2026-02-26 18:02:03 -0300

    Create Discord.js

---
+++ Assets/JS/Discord.js
@@ -0,0 +1,5 @@
+const discordLink = "https://discord.gg/vfEx2aTwY9";
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("discord-btn").href = discordLink;
+});
